41985351bd839ae287f4b396c27fa415f1281ab3
Gary Hake <garyhake@gmail.com>
2022-05-31 21:26:17 -0400

    cleanup

--- 01-getting-started/02-with-vue/app.js
+++ 01-getting-started/02-with-vue/app.js
@@ -1,16 +1,3 @@
-// const buttonEl = document.querySelector('button');
-// const inputEl = document.querySelector('input');
-// const listEl = document.querySelector('ul');
-
-// const addGoal = () => {
-// 	const enteredValue = inputEl.value;
-// 	const listItemEl = document.createElement('li');
-// 	listItemEl.textContent = enteredValue;
-// 	listEl.appendChild(listItemEl);
-// };
-
-// buttonEl.addEventListener('click', addGoal);
-
 Vue.createApp({
 	data() {
 		return {
