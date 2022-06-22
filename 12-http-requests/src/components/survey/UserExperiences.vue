320dfbc34a9dc1bebd4614945b0b97858cdad47d
Gary Hake <garyhake@gmail.com>
2022-06-22 10:23:43 -0400

    getting data

--- 12-http-requests/src/components/survey/UserExperiences.vue
+++ 12-http-requests/src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
 		<base-card>
 			<h2>Submitted Experiences</h2>
 			<div>
-				<base-button>Load Submitted Experiences</base-button>
+				<base-button @click="loadExperiences"
+					>Load Submitted Experiences</base-button
+				>
 			</div>
 			<ul>
 				<survey-result
@@ -24,6 +26,35 @@
 		props: ['results'],
 		components: {
 			SurveyResult,
+		},
+		data() {
+			return {
+				results: [],
+			};
+		},
+		methods: {
+			loadExperiences() {
+				fetch(
+					'https://vue-http-demo-2178f-default-rtdb.firebaseio.com/surveys.json'
+				)
+					.then((response) => {
+						if (response.ok) {
+							return response.json();
+						}
+					})
+					.then((data) => {
+						const results = [];
+						for (const id in data) {
+							results.push({
+								id: id,
+								name: data[id].name,
+								rating: data[id].rating,
+							});
+						}
+
+						this.results = results;
+					});
+			},
 		},
 	};
 </script>
