290407f38e414feddf67d6382d7aa5c0de690965
Gary Hake <garyhake@gmail.com>
2022-06-26 21:15:43 -0400

    initial project

---
+++ 14-animation-transition/src/App.vue
@@ -0,0 +1,117 @@
+<script setup>
+	import BaseModal from './components/BaseModal.vue';
+	import { ref } from 'vue';
+
+	const dialogIsVisible = ref(false);
+	const animatedBlock = ref(false);
+	const paraIsVisible = ref(false);
+
+	const showDialog = () => {
+		dialogIsVisible.value = true;
+	};
+
+	const hideDialog = () => {
+		dialogIsVisible.value = false;
+	};
+
+	const animateBlock = () => {
+		animatedBlock.value = true;
+	};
+
+	const toggleParagraph = () => {
+		paraIsVisible.value = !paraIsVisible.value;
+	};
+</script>
+
+<template>
+	<div class="container">
+		<div class="block" :class="{ animate: animatedBlock }"></div>
+		<button @click="animateBlock">Animate</button>
+	</div>
+
+	<div class="container">
+		<transition>
+			<p v-if="paraIsVisible">This is only sometimes visible...</p>
+		</transition>
+
+		<button @click="toggleParagraph">Toggle Paragraph</button>
+	</div>
+
+	<BaseModal @close="hideDialog" v-if="dialogIsVisible">
+		<p>This is a test dialog!</p>
+		<button @click="hideDialog">Close it!</button>
+	</BaseModal>
+
+	<div class="container">
+		<button @click="showDialog">Show Dialog</button>
+	</div>
+</template>
+
+<style>
+	* {
+		box-sizing: border-box;
+	}
+
+	html {
+		font-family: sans-serif;
+	}
+
+	body {
+		margin: 0;
+	}
+
+	button {
+		font: inherit;
+		padding: 0.5rem 2rem;
+		border: 1px solid #810032;
+		border-radius: 30px;
+		background-color: #810032;
+		color: white;
+		cursor: pointer;
+	}
+
+	button:hover,
+	button:active {
+		background-color: #a80b48;
+		border-color: #a80b48;
+	}
+
+	.block {
+		width: 8rem;
+		height: 8rem;
+		background-color: #290033;
+		margin-bottom: 2rem;
+		/* transition: transform 0.3s ease-out; */
+	}
+
+	.container {
+		max-width: 40rem;
+		margin: 2rem auto;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		padding: 2rem;
+		border: 2px solid #ccc;
+		border-radius: 12px;
+	}
+
+	.animate {
+		/* transform: translateX(-150px); */
+		animation: slide-fade 0.3s ease-out forwards;
+	}
+
+	@keyframes slide-fade {
+		0% {
+			transform: translateX(0) scale(1);
+		}
+
+		70% {
+			transform: translateX(-120px) scale(1.1);
+		}
+
+		100% {
+			transform: translateX(-150px) scale(1);
+		}
+	}
+</style>
