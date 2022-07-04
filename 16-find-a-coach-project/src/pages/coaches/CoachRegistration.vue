23e4018447f7a718257e4aacf8f40652ed8e86ee
Gary Hake <garyhake@gmail.com>
2022-07-04 08:18:48 -0400

    adding coach form

--- 16-find-a-coach-project/src/pages/coaches/CoachRegistration.vue
+++ 16-find-a-coach-project/src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,13 @@
-<script setup></script>
+<script setup>
+	import CoachForm from '../../components/coaches/CoachForm.vue';
+	import BaseCard from '../../components/ui/BaseCard.vue';
+</script>
 
-<template>REGISTER</template>
+<template>
+	<section>
+		<BaseCard>
+			<h2>Register as a coach now!</h2>
+			<CoachForm></CoachForm>
+		</BaseCard>
+	</section>
+</template>
